7abee60c4375fddb0b793769c8ad9ebe8467636c
faithngotho <faithwangari248@gmail.com>
2019-08-05 08:27:24 +0300

    add modified files

--- return/models.py
+++ return/models.py
@@ -1,61 +1,22 @@
-from django.conf import settings
-from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
 
 class Profile(models.Model):
-    '''
-    profile class holding all the models
-    '''
-    username = models.OneToOneField(
-        settings.AUTH_USER_MODEL)
-    profile_picture = ImageField(
-    bio = models.TextField(default="Tell us more")
-    contact = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.username.username
-
-
-def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
-    if created:
-        try:
-            Profile.objects.create(user=instance)
-        except Exception as error:
-            print(error)
-
-post_save.connect(post_save_user_model_receiver,
-                  sender=settings.AUTH_USER_MODEL)
+    image = models.ImageField(default = 'default.jpg',upload_to='images/')
+    bio = models.CharField(max_length=150)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True, related_name='profile') 
 
 
 class Projects(models.Model):
-    title = models.CharField(User, max_length=200)
-    image = ImageField(
-        manual_crop='1280x720')
-    image2 = ImageField(null=True, manual_crop='1280x720')
-    image3 = ImageField(null=True, manual_crop='1280x720')
-    decription = models.TextField(max_length=200)
-    link = models.URLField(null=True, blank=True, default='')
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, related_name="images")
-
-    @classmethod
-    def get_all_projects(cls):
-        projects = Projects.objects.all()
-        return projects
-
-    @classmethod
-    def get_post(cls, id):
-        projects = Projects.objects.filter(user=id)
-        return projects
-
-    class Meta:
-        ordering = ['-id']
-
-    def __str__(self):
-        return self.title
-
-
-class Categories(models.Model):
-    name = models.ManyToManyField(Projects)
+    title = models.CharField(max_length =30)
+    image = models.ImageField(default = 'default.jpg',upload_to='images/')
+    description = models.CharField(max_length =200)
+    link = models.URLField()
+    ratings = models.IntegerField(blank=True, null=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE) 
